2f90306c8ef36e18aebccfb08906adbd3bfc8ec0
jaminchen <oopsplus@vip.qq.com>
2019-02-09 22:47:57 +0800

    fix bug for timeline showing

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -33,7 +33,19 @@
             </a>
             <span> • </span>
             <i class="far fa-clock"></i>
-            <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+            <span class="timeago" title="最后活跃于：
+              @if(if_query('order', 'recent'))
+              {{ $topic->created_at }}
+              @else
+              {{ $topic->updated_at }}
+              @endif
+            ">
+              @if(if_query('order', 'recent'))
+              {{ $topic->created_at->diffForHumans() }}
+              @else
+              {{ $topic->updated_at->diffForHumans() }}
+              @endif
+            </span>
           </small>
 
         </div>
