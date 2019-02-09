2971567450c6cf3312b68f48d41f110d62e3dadf
jaminchen <oopsplus@vip.qq.com>
2019-02-09 22:14:22 +0800

    topic in specified order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,12 +14,17 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	// public function index()
+	// {
+    //     //$topics = Topic::paginate();
+    //     //$topics = Topic::with('user', 'category')->paginate(30);
+	// 	return view('topics.index', compact('topics'));
+    // }
+    public function index(Request $request, Topic $topic)
 	{
-        //$topics = Topic::paginate();
-        $topics = Topic::with('user', 'category')->paginate(30);
+        $topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
-	}
+    }
 
     public function show(Topic $topic)
     {
