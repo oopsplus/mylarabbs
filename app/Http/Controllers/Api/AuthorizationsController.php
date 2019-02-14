6ee23d6a4f0460d8bc30dad27fb56bdb42527cf8
jaminchen <oopsplus@vip.qq.com>
2019-02-14 23:56:36 +0800

    social authorizations

---
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\SocialAuthorizationRequest;
+
+class AuthorizationsController extends Controller
+{
+    //
+}
